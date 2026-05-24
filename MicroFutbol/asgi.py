cf90235503a926fc29700c4628e0e0c9006bdb96
alejo <agudelochacon@gmail.com>
2026-05-24 14:29:56 -0500

    Corregir duplicacion de get_asgi_application en asgi.py

--- MicroFutbol/asgi.py
+++ MicroFutbol/asgi.py
@@ -12,13 +12,16 @@ import os
 from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MicroFutbol.settings')
 
+django_asgi_app = get_asgi_application()
+
 from channels.routing import URLRouter,ProtocolTypeRouter
 from channels.auth import AuthMiddlewareStack
+
 import Home.routing
 import miequipo.routing
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             Home.routing.websocket_urlpatterns +
